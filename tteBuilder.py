81995bb6a36275fca7608efd3094fd529ef572b5
Michael Burgess <jmichaelburgess@gmail.com>
2013-12-21 22:41:34 -0600

    finished TTE maker

--- tteBuilder.py
+++ tteBuilder.py
@@ -3,11 +3,26 @@ from numpy import load as npload
 
 
 
+eboundDict = {'NAI_00': 'n0' ,'NAI_01': 'n1'  , 'NAI_02': 'n2'  , 'NAI_03': 'n3' , 'NAI_04': 'n4' , 'NAI_05': 'n5' , 'NAI_06': 'n6' , 'NAI_07': 'n7' , 'NAI_08': 'n8' , 'NAI_09': 'n9' , 'NAI_10': 'na'  , 'NAI_11': 'nb' , 'BGO_00': 'b0'  , 'BGO_01': 'b1'}
+
+
+
 class tteBuilder(object):
 
-    def __init__(self,fileName):
+    def __init__(self,fileName,evts,chans,det,simInfo):
         
         self.fileName =fileName
+        self.det = det
+        self.evts = evts
+        self.chans = chans
+        self.simInfo  =simInfo
+        self.ebounds = npload(eboundDict[det]+'_bounds.npy')
+
+        
+
+
+
+
         self.bgo = False
         self.nai=True
 
@@ -32,11 +47,11 @@ class tteBuilder(object):
         primaryHeader["TIMEUNIT"] = 's       '
         primaryHeader["MJDREFI"] = 51910
         primaryHeader["MJDREFF"] = 7.428703703703703E-4
-        primaryHeader["TSTART"] = 243216740.670344
-        primaryHeader["TSTOP"] = 243217067.272056
+        primaryHeader["TSTART"] = self.tStart
+        primaryHeader["TSTOP"] = self.tStop
         primaryHeader["FILENAME"] = self.fileName
         primaryHeader["DATATYPE"] = 'TTE     '
-        primaryHeader["TRIGTIME"] = 243216766.613542
+        primaryHeader["TRIGTIME"] = self.trigT
         primaryHeader["OBJECT"] = 'GRB000000000'
         primaryHeader["RADECSYS"] = 'FK5     '
         primaryHeader["EQUINOX"] = 2000.0
@@ -45,23 +60,19 @@ class tteBuilder(object):
         primaryHeader["ERR_RAD"] = 3.000
         primaryHeader["INFILE01"] = 'glg_lutcs_nai_080915695_v00.fit'
         primaryHeader["INFILE02"] = 'GLAST_2008260_074300_VC09_GBTTE.0.00'
-        primaryHeader["CHECKSUM"] = 'PZaEQWTCPWZCPWZC'
-        primaryHeader["DATASUM"] = '         0'
+      #  primaryHeader["CHECKSUM"] = 'PZaEQWTCPWZCPWZC'
+      #  primaryHeader["DATASUM"] = '         0'
 
         self.prihdu = pf.PrimaryHDU(header=primaryHeader)
 
 
     def _MakeEboundsEXT(self):
 
-        if self.bgo:
-            ebounds = npload('ebounds_bgo.npy')
-        if self.nai:
-            ebounds = npload('ebounds_nai.npy')
+        
 
-
-        channel = pf.Column(name='CHANNEL ', format='1I      ', unit='none', array=ebounds[:,0])
-        emin =  pf.Column(name='E_MIN   ', format='1E      ', unit='keV     ', array=ebounds[:,1])
-        emax =  pf.Column(name='E_MAX   ', format='1E      ', unit='keV     ', array=ebounds[:,2])
+        channel = pf.Column(name='CHANNEL ', format='1I', unit='none', array=self.ebounds[:,0])
+        emin =  pf.Column(name='E_MIN   ', format='1E', unit='keV     ', array=self.ebounds[:,1])
+        emax =  pf.Column(name='E_MAX   ', format='1E', unit='keV     ', array=self.ebounds[:,2])
 
 
         cols = pf.ColDefs([channel,emin,emax])
@@ -69,10 +80,10 @@ class tteBuilder(object):
         tbhdu=pf.new_table(cols,tbtype='BinTableHDU')
         
         header=tbhdu.header
-
+        header["EXTNAME"] = 'EBOUNDS '
         header["TELESCOP"] = 'GLAST   '
         header["INSTRUME"] = 'GBM     '
-        header["DETNAM"] = 'NAI_03  '
+        header["DETNAM"] = self.det
         header["OBSERVER"] = 'Meegan  '
         header["ORIGIN"] = 'GIOC    '
         header["DATE"] = '2009-05-19T18:49:32'
@@ -82,21 +93,23 @@ class tteBuilder(object):
         header["TIMEUNIT"] = 's       '
         header["MJDREFI"] = 51910
         header["MJDREFF"] = 7.428703703703703E-4
-        header["TSTART"] = 243216740.670344
-        header["TSTOP"] = 243217067.272056
-        header["FILENAME"] = self.fileName
+        header["TSTART"] = self.tStart
+        header["TSTOP"] = self.tStop
         header["DATATYPE"] = 'TTE     '
-        header["TRIGTIME"] = 243216766.613542
+        header["TRIGTIME"] = self.trigT
         header["OBJECT"] = 'GRB000000000'
         header["RADECSYS"] = 'FK5     '
         header["EQUINOX"] = 2000.0
         header["RA_OBJ"] = 30.0000
         header["DEC_OBJ"] = -15.000
         header["ERR_RAD"] = 3.000
-        header["INFILE01"] = 'glg_lutcs_nai_080915695_v00.fit'
-        header["INFILE02"] = 'GLAST_2008260_074300_VC09_GBTTE.0.00'
-        header["CHECKSUM"] = 'PZaEQWTCPWZCPWZC'
-        header["DATASUM"] = '         0'
+        header["CHANTYPE"] = 'PHA     '
+        header["DETCHANS"] = 128
+        header["EXTVER"] = 1
+        header["CH2E_VER"] ='SPLINE 2.0'
+        header["GAIN_COR"] = 1.0
+      #  header["CHECKSUM"] = 'PZaEQWTCPWZCPWZC'
+      #  header["DATASUM"] = '         0'
 
 
 
@@ -111,23 +124,23 @@ class tteBuilder(object):
             return
 
        
+        tz = 243216766.613542
 
-
-        pha  = pf.Column(name= 'PHA     ', format='1I      ', unit='none', array=channels)
-        time = pf.Column(name= 'TIME    ', format='1D      ', unit='s       ', array=events)
+        pha  = pf.Column(name= 'PHA     ', format='1I', unit='none', array=channels)
+        time = pf.Column(name= 'TIME    ', format='1D', unit='s       ', array=events-tz)
       
-
+        
 
         cols = pf.ColDefs([time,pha])
 
         tbhdu=pf.new_table(cols,tbtype='BinTableHDU')
         
         header=tbhdu.header
-        header["TZERO1"]=243216766.613542
+        header["TZERO1"]=self.tz
         header["EXTNAME"] = 'EVENTS  '
         header["TELESCOP"] = 'GLAST   '
         header["INSTRUME"] = 'GBM     '
-        header["DETNAM"] = 'NAI_03  '
+        header["DETNAM"] = self.det
         header["OBSERVER"] = 'Meegan  '
         header["ORIGIN"] = 'GIOC    '
         header["DATE"] = '2009-05-19T18:49:32'
@@ -137,11 +150,13 @@ class tteBuilder(object):
         header["TIMEUNIT"] = 's       '
         header["MJDREFI"] = 51910
         header["MJDREFF"] = 7.428703703703703E-4
-        header["TSTART"] = 243216740.670344
-        header["TSTOP"] = 243217067.272056
-        header["FILENAME"] = self.fileName
+        
+        header["TSTART"] = self.tStart
+        header["TSTOP"] = self.tStop
         header["DATATYPE"] = 'TTE     '
-        header["TRIGTIME"] = 243216766.613542
+        header["TRIGTIME"] = self.trigT
+        
+        
         header["OBJECT"] = 'GRB000000000'
         header["RADECSYS"] = 'FK5     '
         header["EQUINOX"] = 2000.0
@@ -154,6 +169,7 @@ class tteBuilder(object):
         header["DETCHANS"]  = 128
         header["HDUCLASS"]  = 'OGIP    '
         header["HDUCLAS1"] = 'EVENTS  '
+        header["EXTVER"] = 1
         header["CHECKSUM"] = 'PZaEQWTCPWZCPWZC'
         header["DATASUM"] = '         0'
 
@@ -164,8 +180,68 @@ class tteBuilder(object):
 
 
 
+    def _MakeGTI(self):
+        
+
+        tz = 243216766.613542
+
+        tstart = pf.Column(name='START   ', format='1D', unit='s', array=[243216740.670344-tz])
+        tstop =  pf.Column(name='STOP    ', format='1D', unit='s     ', array=[243217067.27205601-tz])
+       
+
+
+        cols = pf.ColDefs([tstart,tstop])
+
+        tbhdu=pf.new_table(cols,tbtype='BinTableHDU')
+        
+        header=tbhdu.header
+        header["EXTNAME"] = 'GTI     ' 
+        header["TELESCOP"] = 'GLAST   '
+        header["INSTRUME"] = 'GBM     '
+        header["DETNAM"] = self.det
+        header["OBSERVER"] = 'Meegan  '
+        header["ORIGIN"] = 'GIOC    '
+        header["DATE"] = '2009-05-19T18:49:32'
+        header["DATE-OBS"] = '2008-09-16T00:12:20'
+        header["DATE-END"] = '2008-09-16T00:17:47'
+        header["TIMESYS"] = 'TT      '
+        header["TIMEUNIT"] = 's       '
+        header["MJDREFI"] = 51910
+        header["MJDREFF"] = 7.428703703703703E-4
+        header["TSTART"] = self.tStart
+        header["TSTOP"] = self.tStop
+        header["DATATYPE"] = 'TTE     '
+        header["TRIGTIME"] = self.trigT
+       
+        header["TZERO1"]=self.tz
+        header["TZERO2"]=self.tz
+       
+        header["OBJECT"] = 'GRB000000000'
+        header["RADECSYS"] = 'FK5     '
+        header["EQUINOX"] = 2000.0
+        header["RA_OBJ"] = 30.0000
+        header["DEC_OBJ"] = -15.000
+        header["ERR_RAD"] = 3.000
+        header["CHANTYPE"] = 'PHA     '
+        header["DETCHANS"] = 128
+        header["EXTVER"] = 1
+        header["HDUCLASS"]  = 'OGIP    '
+        header["HDUCLAS1"] = 'GTI     '
+        header["HDUVERS"] = '1.2.0'
+        header["EXTVER"] = 1
+       
+       # header["CHECKSUM"] = 'PZaEQWTCPWZCPWZC'
+       # header["DATASUM"] = '         0'
+
+
+
+
+        self.gtihdu = tbhdu
+        
+
 
     def _MakeHDUList(self):
 
-        hduList = pf.HDUList([self.prihdu, self.eboundshdu,self.eventshdu])
-        hduList.writeto(self.fileName)
+        self.hduList = pf.HDUList([self.prihdu, self.eboundshdu,self.eventshdu, self.gtihdu])
+        self.hduList.writeto(self.fileName,clobber=True)
+        
