3c4d93b77c14522801f4e84425ade5d83df057aa
jburgess <>
2014-01-23 18:02:29 +0100

    added gers code to drm

--- rsp.py
+++ rsp.py
@@ -1,5 +1,6 @@
 import astropy.io.fits as pf
 from numpy import zeros, array, matrix
+import math
 
 class rsp(object):
 
@@ -8,17 +9,27 @@ class rsp(object):
 
         rspFile = pf.open(rspFileName)
     
+        angle = 67 #This is temporarily hardcoded and should be updated 
+
 
         self.chanData = rspFile[1].data
         self.numEnergyBins = rspFile[2].header['NUMEBINS']
         self.numDetChans = rspFile[2].header['DETCHANS']
         self.det = rspFile[0].header['DETNAM']
         
+        self.beta = angle
+        self.photonE = (rspFile[2].data["ENERG_LO"], rspFile[2].data["ENERG_HI"])
+	self.channelE = (rspFile[1].data["E_MIN"], rspFile[1].data["E_MAX"])
+        
         #Main component of object
         self.drm = zeros((self.numEnergyBins, self.numDetChans))
 
         self._ConstructDRM(rspFile)
         self.binWidths = rspFile[1].data["E_MAX"]-rspFile[1].data["E_MIN"]
+
+        if self.beta:
+            self._CalculateProb()
+
 
         rspFile.close()
 
@@ -47,3 +58,26 @@ class rsp(object):
         del mData
 
         
+    def _CalculateProb(self):
+            '''
+            Convert the drm into a probablility. To do so, we need the angle 
+            relative to the normal of the detector. For the NaI the 
+            angle should be relative to the face of the cylinder. For the BGO
+            it is given relative to the side of the detector.
+            '''
+
+            area = lambda r, h, b: math.fabs(math.pi *(r**2) * math.cos(math.radians(b))) + math.fabs(2 * r * h * math.sin(math.radians(b)))
+
+            if self.det[0].lower() == "n":
+                    # NaI's are cylinders of diameter 12.7 cm and thickness 1.27 cm
+                    geoArea = area(12.7/2., 1.27, self.beta)
+            else:
+                    # BGO's are cylinders of diameter 12.7 cm and thickness 12.7 cm.
+                    # For the BGO, we have to add an offset of 90 deg to the angle. This
+                    # is because for the NaI the angles are calculate relative to the face
+                    # of the cylinder whereas for the BGO they are calculated relative to the 
+                    # cylindrical face.
+                    geoArea = area(12.7/2., 12.7, self.beta + 90.)
+            self.geoArea = geoArea
+            self.prob = self.drm/geoArea
+            # print "Det: %s, Beta: %.1f
